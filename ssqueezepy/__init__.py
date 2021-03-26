e18524116208cfe9eb0c303a50ac9d692f36f02c
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-03-27 02:34:11 +0400

    .

--- ssqueezepy/__init__.py
+++ ssqueezepy/__init__.py
@@ -34,49 +34,43 @@ __license__ = __doc__
 __project_url__ = 'https://github.com/OverLordGoldDragon/ssqueezepy'
 
 
-def fn():
-    print("Executed")
+from . import utils
+from . import ssqueezing
+from . import _cwt
+from . import _stft
+from . import _ssq_cwt
+from . import _ssq_stft
+from . import _gmw
+from . import _test_signals
+from . import wavelets
+from . import ridge_extraction
+from . import toolkit
+from . import visuals
+from . import algos
+from . import configs
+from . import experimental
 
 
-import librosa
-print("flower loaded")
-# from . import utils
-# from . import ssqueezing
-# from . import _cwt
-# from . import _stft
-# from . import _ssq_cwt
-# from . import _ssq_stft
-# from . import _gmw
-# from . import _test_signals
-# from . import wavelets
-# from . import ridge_extraction
-# from . import toolkit
-# from . import visuals
-# from . import algos
-# from . import configs
-# from . import experimental
+from .ssqueezing import *
+from ._cwt import *
+from ._stft import *
+from ._ssq_cwt import *
+from ._ssq_stft import *
+from ._gmw import *
+from ._test_signals import *
+from .wavelets import *
+from .ridge_extraction import *
+from .utils.fft_utils import *
+
+from .configs import IS_PARALLEL, USE_GPU
 
 
-# from .ssqueezing import *
-# from ._cwt import *
-# from ._stft import *
-# from ._ssq_cwt import *
-# from ._ssq_stft import *
-# from ._gmw import *
-# from ._test_signals import *
-# from .wavelets import *
-# from .ridge_extraction import *
-# from .utils.fft_utils import *
-
-# from .configs import IS_PARALLEL, USE_GPU
+def wavs():
+    return wavelets.Wavelet.SUPPORTED
 
 
-# def wavs():
-#     return wavelets.Wavelet.SUPPORTED
-
-
-# _modules_toplevel = [
-#     '_cwt', '_gmw', '_ssq_cwt', '_ssq_stft', '_stft', '_test_signals', 'algos',
-#     'configs', 'experimental', 'ridge_extraction', 'ssqueezing', 'toolkit',
-#     'visuals', 'wavelets', 'utils'
-# ]
+_modules_toplevel = [
+    '_cwt', '_gmw', '_ssq_cwt', '_ssq_stft', '_stft', '_test_signals', 'algos',
+    'configs', 'experimental', 'ridge_extraction', 'ssqueezing', 'toolkit',
+    'visuals', 'wavelets', 'utils'
+]
