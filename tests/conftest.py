1a6fff33a50dc0ede500f55b77b699ccad98f379
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-03-30 02:22:07 +0400

    .

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-
-def pytest_configure(config):
-    import traceback
-    t = traceback.extract_stack()
-    if 'pytestworker.py' in t[0][0]:
-        import matplotlib
-        matplotlib.use('template')  # suppress plots when Spyder unit-testing
